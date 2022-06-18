e8a5d7d7b121b99784c5e97d85febad87a388ef1
Matheus <matheus.patricio@sptech.school>
2022-06-18 11:36:25 -0300

    Modificações que tinha feito no dia da entrega nas validações

--- site/src/database/script-tabelas.sql
+++ site/src/database/script-tabelas.sql
@@ -7,16 +7,7 @@ nome varchar(120),
 email varchar(120),
 senha varchar(100),
 idade int);
-create table questoes(
-idQuestao int primary key auto_increment,
-pergunta varchar(100),
-alternativaA varchar(80),
-alternativaB varchar(80),
-alternativaC varchar(80),
-alternativaD varchar(80),
-resposta char(1)
-check (resposta = 'A' or resposta = 'B' or resposta = 'C' or resposta = 'D')
-);
+
 
 
 create table pontos(
